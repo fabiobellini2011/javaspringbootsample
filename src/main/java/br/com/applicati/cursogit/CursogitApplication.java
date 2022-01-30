dd34520a3c4d8165b9f67f6f3a2d5a24a751eb19
Fabio Bellini <fabiobellini1975@outlook.com>
2022-01-30 17:49:49 -0300

    Enviando projeto ao GitHub

---
+++ src/main/java/br/com/applicati/cursogit/CursogitApplication.java
@@ -0,0 +1,13 @@
+package br.com.applicati.cursogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursogitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/applicati/cursogit/CursogitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.applicati.cursogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
